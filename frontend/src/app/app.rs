1976d9f687f6f52bbc2ac806973c11736aab61a4
MaloGuillou <malo.guillou@et.esiea.fr>
2026-06-23 19:29:13 +0200

    Add protected routes for authentication

--- frontend/src/app/app.rs
+++ frontend/src/app/app.rs
@@ -4,7 +4,7 @@ use leptos_router::{components::*, path};
 use crate::components::navbar::nav::Navbar;
 use crate::pages::home::home::Home;
 use crate::pages::{login::login::Login, register::register::Register};
-use crate::store::auth::provide_auth_state;
+use crate::store::auth::{get_current_user, provide_auth_state};
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -17,9 +17,28 @@ pub fn App() -> impl IntoView {
             <main class="p-4">
                 // The Router looks at the URL and renders only the matching Route
                 <Routes fallback=|| view! { <h1>"404 Not Found"</h1> }>
-                    <Route path=path!("/") view=Home />
-                    <Route path=path!("/login") view=Login />
-                    <Route path=path!("/register") view=Register />
+                    <Route
+                        path=path!("/")
+                        view=Home
+                    />
+                    <ProtectedRoute
+                        path=path!("/login")
+                        redirect_path=|| "/chat"
+                        condition=move || Some(!get_current_user().get().is_some())
+                        view=Login
+                    />
+                    <ProtectedRoute
+                        path=path!("/register")
+                        redirect_path=|| "/chat"
+                        condition=move || Some(!get_current_user().get().is_some())
+                        view=Register
+                    />
+                    <ProtectedRoute
+                        path=path!("/chat")
+                        redirect_path=|| "/login"
+                        condition=move || Some(get_current_user().get().is_some())
+                        view=|| view! { <h1>"Private Chat UI"</h1> }
+                    />
                 </Routes>
             </main>
         </Router>
